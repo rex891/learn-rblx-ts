24026809033740db0337225f450d8a080f2783b5
Chavez, Joe <chavez35@llnl.gov>
2025-11-07 07:53:31 -0800

    fix: adjust formatting in JSX intrinsic elements for consistency

--- src/react-app.d.ts
+++ src/react-app.d.ts
@@ -2,13 +2,13 @@
 /// <reference types="@rbxts/compiler-types" />
 
 declare namespace JSX {
-    interface IntrinsicElements {
-        screengui: Partial<ScreenGui> & { children?: React.ReactNode };
-        frame: Partial<Frame> & { children?: React.ReactNode };
-        textlabel: Partial<TextLabel> & { children?: React.ReactNode };
-        textbutton: Partial<TextButton> & { children?: React.ReactNode };
-        uipadding: Partial<UIPadding>;
-        uicorner: Partial<UICorner>;
-        folder: Partial<Folder> & { children?: React.ReactNode };
-    }
+	interface IntrinsicElements {
+		screengui: Partial<ScreenGui> & { children?: React.ReactNode }
+		frame: Partial<Frame> & { children?: React.ReactNode }
+		textlabel: Partial<TextLabel> & { children?: React.ReactNode }
+		textbutton: Partial<TextButton> & { children?: React.ReactNode }
+		uipadding: Partial<UIPadding>
+		uicorner: Partial<UICorner>
+		folder: Partial<Folder> & { children?: React.ReactNode }
+	}
 }
